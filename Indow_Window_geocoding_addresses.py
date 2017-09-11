2a0075b444408a6f395ff2bb7ac03dcc5bedef58
William Brown <etokrug@gmail.com>
2017-09-10 19:32:22 -0700

    Yo, check it, csv party
    This may not work but it MIGHT try and open a csv file and print stuff into it.

--- Indow_Window_geocoding_addresses.py
+++ Indow_Window_geocoding_addresses.py
@@ -4,6 +4,7 @@ from pygeocoder import Geocoder
 import pandas as pd
 import numpy as np
 import os
+import csv
 
 wrks = (r"/home/gustacro/Desktop")
 database = os.path.join(wrks,"02_Historic_places_US.csv")
@@ -19,15 +20,28 @@ df['Address'] = df['full_address']
 # view Address that we are going to get the latitude and longitude
 # print(address)
 # Verify that an address is valid (i.e. in Google's system)
-for Address in df['full_address']:
-    print(Address)
-    address_validation = Geocoder.geocode(Address).valid_address
 
-# view if address is valid
-    print(address_validation)
-
-# save the address into a a new variable
-    result_address = Geocoder.geocode(Address)
-
-# print coordinates for the address given.
-    print(result_address.coordinates)
+with open('outfile.csv', 'w') as newfile:
+    wr = csv.writer(newfile, quoting=csv.QUOTE_ALL)
+    for index, row in df.iterrows():
+        objectid = row['c1']
+        loc = row['c2']
+        city = row['c3']
+        state = row['c4']
+        full_address = row['c5']
+        
+        address_validation = Geocoder.geocode(full_address).valid_address
+        
+        # view if address is valid
+        print(address_validation)
+    
+        # save the address into a a new variable
+        result_address = Geocoder.geocode(full_address)
+        lat = result_address.coordinates[0]
+        lng = result_address.coordinates[1]
+    
+        # print coordinates for the address given.
+        print(result_address.coordinates)
+        final_list = [lat, lng, full_address]
+        print(final_list)
+        wr.writerow(final_list)
